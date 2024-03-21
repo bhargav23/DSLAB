4cfdda7c5808dfa2320b5d37c798be2e146c2ca6
bhargav23 <bhargav.kiran@gmail.com>
2024-03-21 11:12:14 +0530

    Update CL1.c

--- CL1.c
+++ CL1.c
@@ -1,119 +1,168 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node {
+struct node{
 	int data;
 	struct node *next;
 };
-struct node* circularList(int data) {
-	struct node *temp = (struct node *)malloc(sizeof(struct node *));
-	temp->data=data;
-	temp->next=temp;
-	return temp;
-}
-void insert_at_beginnig(struct node *tail,int data) {
-	struct node *temp = (struct node *)malloc(sizeof(struct node *));
-	temp->data=data;
-	temp->next=tail->next;
-	tail->next=temp;
-}
-void display(struct node *tail) {
-	if(tail==NULL)
-	{
-		printf("\nList is empty\n");
-		return;	
-	}	
-	struct node *ptr = tail->next;
+void display(struct node *tail){
+	if(tail==NULL){
+		printf("\nList is empty");
+		return;
+	}
+	printf("\n");
+	struct node *ptr=tail->next;
 	do{
 		printf("%d ",ptr->data);
 		ptr=ptr->next;
 	}while(ptr!=tail->next);
-	printf("\n");
-	return;
 }
-void insert_at_end(struct node **tail,int data) {
-	struct node *temp = (struct node *)malloc(sizeof(struct node *));
-	temp->data=data;
-	temp->next=(*tail)->next;
-	(*tail)->next=temp;
-	*tail=temp; 
+void create_node(struct node **tail,int d){
+	struct node *temp=(struct node *)malloc(sizeof(struct node *));
+	temp->data=d;
+	temp->next=temp;
+	*tail=temp;
 }
-void insert_at_position(struct node **tail,int data,int pos){
-	if(pos==1){
-		insert_at_beginnig(*tail,data);
-		return;
-	} 
-	struct node *ptr=(*tail)->next;
-	--pos;
-	while(pos>1){
-		ptr=ptr->next;
-		pos--;
-	}
-	if(ptr==*tail) {
-		insert_at_end(tail,data);
+void insert_at_beginning(struct node **tail,int d){
+	if(*tail==NULL){
+		printf("\nList is empty");
 		return;
 	}
 	struct node *temp=(struct node *)malloc(sizeof(struct node *));
-	temp->data=data;
-	temp->next=NULL;
+	temp->data=d;
+	temp->next=(*tail)->next;
+	(*tail)->next=temp;
+}
+void insert_at_end(struct node **tail,int d){
+	if(*tail==NULL){
+		printf("\nList is empty");
+		return;
+	}
+	struct node *temp=(struct node *)malloc(sizeof(struct node *));
+	temp->data=d;
+	temp->next=(*tail)->next;
+	(*tail)->next=temp;
+	(*tail)=temp;
+}
+void insert_at_position(struct node **tail,int d,int pos){
+	if(*tail==NULL){
+		printf("\nList is empty");
+		return;
+	}
+	if(pos==1){
+		insert_at_beginning(tail,d);
+		return;
+	}
+	pos--;
+	struct node *ptr=(*tail)->next;
+	while(pos>1){
+		ptr=ptr->next;
+		pos--;
+		if(ptr==(*tail)->next)
+		{
+			printf("Invalid Position\n");
+			return;	
+		}
+	}
+	if(ptr==(*tail)){
+		insert_at_end(tail,d);
+		return;
+	}
+	struct node *temp=(struct node *)malloc(sizeof(struct node *));
+	temp->data=d;
 	temp->next=ptr->next;
 	ptr->next=temp;
 }
-void delete_at_beginnig(struct node **tail) {
-	if(*tail==NULL)
+void delete_at_beginning(struct node **tail){
+	if(*tail==NULL){
+		printf("\nList is empty");
 		return;
-	struct node *ptr = (*tail)->next;
-	if(ptr==*tail)
-	{
-		
+	}
+	struct node *ptr=(*tail)->next;
+	if(ptr->next==ptr){
 		free(ptr);
-		(*tail)=NULL;
+		*tail=NULL;
 		return;
 	}
 	(*tail)->next=ptr->next;
-	ptr->next=NULL;
 	free(ptr);
-	
 }
 
-void delete_at_end(struct node **tail) {
-	if(*tail==NULL)
-	{
-		printf("\nList is empty\n");
-		return;	
-	}	
-	struct node *ptr = (*tail)->next;
-	if(ptr==*tail) {
-		free(ptr);
-		(*tail)=NULL;
+void delete_at_end(struct node **tail){
+	if(*tail==NULL){
+		printf("\nList is empty");
 		return;
 	}
-	while(ptr->next!=*tail){
+	struct node *ptr=*tail;
+	if(ptr->next==ptr){
+		free(ptr);
+		*tail=NULL;
+		return;
+	}
+	
+	while(ptr->next!=*tail) {
 		ptr=ptr->next;
 	}
+	struct node *temp=*tail;
 	ptr->next=(*tail)->next;
-	free(*tail);
 	*tail=ptr;
-	free(ptr->next);
-	
+	free(temp);
+}
 
-} 
-int main() {
-	struct node *tail;
-	tail = circularList(10);
-	insert_at_beginnig(tail,20);
-	insert_at_beginnig(tail,30);
-	insert_at_beginnig(tail,40);
-	insert_at_end(&tail,50);
+void delete_at_position(struct node **tail,int pos){
+	if(*tail==NULL){
+		printf("\nList is empty");
+		return;
+	}
+	struct node *ptr=(*tail)->next;
+	if(ptr->next==ptr){
+		free(ptr);
+		*tail=NULL;
+		return;
+	}
+	pos--;
+	while(pos>1){
+		ptr=ptr->next;
+		pos--;
+		if(ptr->next==(*tail)->next){
+			printf("\nInvalid Position");
+			return;
+		}
+	}
+	struct node *ptr1=ptr->next;
+	ptr->next=ptr1->next;
+	if(ptr1==*tail){
+		(*tail)=ptr;
+		free(ptr1);
+		return;
+	}
+}
+
+
+int main(){
+	struct node *tail=NULL;
+	create_node(&tail,10);
 	display(tail);
-	insert_at_position(&tail,60,1);
+
+
+	
+	insert_at_beginning(&tail,20);
 	display(tail);
-	insert_at_position(&tail,70,7);
+	insert_at_end(&tail,30);
 	display(tail);
-	insert_at_position(&tail,80,3);
+	insert_at_position(&tail,40,1);
 	display(tail);
-	delete_at_beginnig(&tail);
+	insert_at_position(&tail,50,5);
 	display(tail);
+	insert_at_position(&tail,60,2);
+	display(tail);
+	delete_at_beginning(&tail);
+	display(tail);
+
 	delete_at_end(&tail);
 	display(tail);
+	
+	delete_at_position(&tail,5);
+	display(tail);
+	
 	
 }
